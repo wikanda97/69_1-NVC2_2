8b62e96cda8ff55f6f9a5bf113ff96f58dd99c82
Wichayada05 <wikandamanok342@gmail.com>
2026-08-27 15:06:49 +0700

    +

---
+++ 048-wikanda/job04.py
@@ -0,0 +1,36 @@
+student1 = {
+    "name": "Wikanda",
+    "last name" : "Manok",
+    "age": 17,
+    "class": "2_2",
+    "phone": "0630016860"
+}
+
+student2 = {
+    "name": "Apinya",
+    "last name" : "Innok",
+    "age": 16,
+    "class": "2_2",
+    "phone": "0651134113"
+}
+
+student3 = {
+    "name": "Nalinthip",
+    "last name" : "Putthisa",
+    "age": 17,
+    "class": "2_2",
+    "phone": "0838258590"
+}
+
+
+class2_2 = [student1, student2, student3]
+
+
+print(class2_2[0])
+print(class2_2[1])
+print(class2_2[2])
+
+
+print("=== รายชื่อเพื่อนสนิท 2 คน ===")
+print("1.", class2_2[1]["name"])
+print("2.", class2_2[2]["name"])
