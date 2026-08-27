c23c5f2635a4a3988f171d061ee9f8b5be2b1c0f
Wichayada05 <wikandamanok342@gmail.com>
2026-08-27 14:48:52 +0700

    +

---
+++ 048-wikanda/่job03.py
@@ -0,0 +1,12 @@
+point = int(input("กรอกคะแนน : "))
+
+if point >= 80 :
+    print("เกรด A")
+elif point >= 70 :
+    print("เกรด B")
+elif point >= 60  :
+    print("เกรด C")
+elif point >= 50  :
+    print("เกรด D")
+elif point < 50   :
+    print("เกรด F")
